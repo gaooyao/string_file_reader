ac2185c6031f37dba1c1895df127bb525dc959c0
三土先森 <gaoyaomail@gmail.com>
2020-10-14 17:16:36 +0800

    Update string_file_reader.h

--- string_file_reader.h
+++ string_file_reader.h
@@ -4,6 +4,7 @@
 * 内容摘要：文本文件读取插件
 * 作    者：gaooyao
 * 邮    箱：1625545050@qq.com
+* 项目地址：https://github.com/gaooyao/string_file_reader
 * 简    介：通过引入缓冲区，减少读取超大文本时访问外存的频率，实现了文件的加速读取，读写性能得到明显提升。
 * 使用方法：1：创建FileHandler指针
 *          2：调用open_file函数，打开文件
